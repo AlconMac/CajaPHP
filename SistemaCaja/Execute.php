c2d90e13f3d11ce20187ce87ae914bd5e275171a
alcon <lalcantata@gmail.com>
2013-05-25 17:45:10 -0500

    jhjhgfj gfjhf

--- SistemaCaja/Execute.php
+++ SistemaCaja/Execute.php
@@ -4,7 +4,7 @@
  *******************************/
 $clase=(isset($_GET["ctr"]))?(string)trim($_GET["ctr"])."Control":"";
 $metodo=(isset($_GET["met"]))?(string)trim($_GET["met"]):"index";
-if($clase!="" && $metodo!=""){//Nuevo Codigo
+if($clase!="" && $metodo!=""){
    if(file_exists("ControlMVC/".$clase.".php")){ 
        include_once "ControlMVC/".$clase.'.php';  
     if(method_exists($clase, $metodo))
